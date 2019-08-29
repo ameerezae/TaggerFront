4f57a611591696b736cbb6a19ffa128dceae9807
96521227 <amir.reazei1999@yahoo.com>
2019-08-29 15:05:00 +0430

    Change font-size of Application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import LoginContainer from "./container/Login/Login"
+import LoginContainer from "./container/Login/Login";
+import SignUpContainer from "./container/SignUp/SignUp";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <div>
           <Switch>
             <Route path="/login" component={LoginContainer}/>
+            <Route path="/signup" component={SignUpContainer} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ src/container/SignUp/SignUp.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class SignUpContainer extends Component {
+    render() {
+        const signUpForm = (
+            <div className="center container">
+                OK
+
+            </div>
+        );
+        return (
+            <div>
+                <div className="background"></div>
+                {signUpForm}
+            </div>
+        );
+    }
+}
+
+export default SignUpContainer;
