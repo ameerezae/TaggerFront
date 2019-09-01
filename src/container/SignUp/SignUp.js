d21fe33a3e4b256d1fa5604f1f95fc8e809983eb
96521227 <amir.reazei1999@yahoo.com>
2019-09-01 12:57:29 +0430

    Add logic to signup page

--- src/container/SignUp/SignUp.js
+++ src/container/SignUp/SignUp.js
@@ -2,6 +2,43 @@ import React, {Component} from 'react';
 import "./SignUp.css";
 import {Button, FormControl, InputGroup} from "react-bootstrap";
 class SignUpContainer extends Component {
+    state = {
+        data : {
+            username : "",
+            email : "",
+            password1: "",
+            password2 : "",
+            phone_number: "",
+        }
+    }
+
+    handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState(prevstate => {
+            const newState = {...this.state};
+            newState.data[name] = value;
+            return newState;
+            }
+        )
+        console.log(this.state.data);
+    };
+    handleSubmit = (event, data) =>{
+        event.preventDefault();
+        const url = "http://127.0.0.1:8000/authentication/registration/";
+
+        fetch(`${url}`,{
+            method : "POST",
+            body : JSON.stringify(data),
+            headers : {
+                "Content-type" : "application/json"
+            }
+        })
+        console.log("ready");
+    };
+
+
     render() {
         const signUpForm = (
             <div className="container signup-body">
@@ -9,92 +46,94 @@ class SignUpContainer extends Component {
                     <p className="signup-title mt-3">ثبت نام </p>
                 </div>
                 <hr/>
-                <div className="row justify-content-end">
-                    <h2 className="id-input-field mt-3">نام کاربری</h2>
-                </div>
-                <div className="row justify-content-center">
-                    <InputGroup  className="mb-3 mt-2 id-input-signup">
-                        <FormControl
-                            // onChange = {}
-                            name ="id"
-                            // value = {this.state.data.Email}
-                            id="text" type="text"
-                            className = "placeHolder-input input-style"
-                            placeholder="نام کاربری مورد نظر خودرا وارد نمایید"
-                        />
-                    </InputGroup>
-                </div>
-                <div className="row justify-content-end">
-                    <h2 className="email-input-field mt-3">ایمیل</h2>
-                </div>
-                <div className="row justify-content-center">
-                    <InputGroup  className="mb-3 mt-2 email-input-signup">
-                        <FormControl
-                            // onChange = {this.hadnleChange}
-                            name ="Email"
-                            // value = {this.state.data.Email}
-                            id="email" type="email"
-                            className = "placeHolder-input input-style"
-                            placeholder="ایمیل خودرا وارد نمایید"
-                        />
-                    </InputGroup>
-                </div>
-                <div className="row justify-content-end">
-                    <h2 className="phoneNumber-input-field mt-3">شماره تلفن همراه</h2>
-                </div>
-                <div className="row justify-content-center">
-                    <InputGroup  className="mb-3 mt-2 phoneNumber-input-signup">
-                        <FormControl
-                            // onChange = {}
-                            name ="Email"
-                            // value = {this.state.data.Email}
-                            id="email" type="phone"
-                            className = "placeHolder-input input-style"
-                            placeholder="شماره تلفن همراه خودرا وارد نمایید"
-                        />
-                    </InputGroup>
-                </div>
-                <div className="row password-box">
-                    <div className="col-sm-6">
-                        <div className="row justify-content-start">
-                            <h2 className="password-input-field mt-4">رمزعبور</h2>
-                        </div>
-                        <div className="row justify-content-center">
-                            <InputGroup className="mb-3 mt-2 password-input-signup">
-                                <FormControl
-                                    // onChange = {this.hadnleChange}
-                                    name = "Password"
-                                    // value = {this.state.data.Password}
-                                    id="password" type="password"
-                                    className = "placeHolder-input input-style"
-                                    placeholder="رمز عبور خودرا وارد نمایید"
-                                />
-                            </InputGroup>
-                        </div>
+                <form onSubmit={(event) => this.handleSubmit(event, this.state.data)}>
+                    <div className="row justify-content-end">
+                        <h2 className="id-input-field mt-3">نام کاربری</h2>
                     </div>
-                    <div className="col-sm-6">
+                    <div className="row justify-content-center">
+                        <InputGroup  className="mb-3 mt-2 id-input-signup" >
+                            <FormControl
+                                onChange = {this.handleChange}
+                                name ="username"
+                                value = {this.state.data.username}
+                                id="username" type="text"
+                                className = "placeHolder-input input-style"
+                                placeholder="نام کاربری مورد نظر خودرا وارد نمایید"
+                            required/>
+                        </InputGroup>
+                    </div>
+                    <div className="row justify-content-end">
+                        <h2 className="email-input-field mt-3">ایمیل</h2>
+                    </div>
+                    <div className="row justify-content-center">
+                        <InputGroup  className="mb-3 mt-2 email-input-signup">
+                            <FormControl
+                                onChange = {this.handleChange}
+                                name ="email"
+                                value = {this.state.data.email}
+                                id="email" type="email"
+                                className = "placeHolder-input input-style"
+                                placeholder="ایمیل خودرا وارد نمایید"
+                                required/>
+                        </InputGroup>
+                    </div>
+                    <div className="row justify-content-end">
+                        <h2 className="phoneNumber-input-field mt-3">شماره تلفن همراه</h2>
+                    </div>
+                    <div className="row justify-content-center">
+                        <InputGroup  className="mb-3 mt-2 phoneNumber-input-signup">
+                            <FormControl
+                                onChange = {this.handleChange}
+                                name ="phone_number"
+                                value = {this.state.data.phone_number}
+                                id="phone_number" type="phone"
+                                className = "placeHolder-input input-style"
+                                placeholder="شماره تلفن همراه خودرا وارد نمایید"
+                                required/>
+                        </InputGroup>
+                    </div>
+                    <div className="row password-box">
+                        <div className="col-sm-6">
                             <div className="row justify-content-start">
-                                <h2 className="password-input-field mt-4"> تکرار رمزعبور</h2>
+                                <h2 className="password-input-field mt-4">رمزعبور</h2>
                             </div>
                             <div className="row justify-content-center">
                                 <InputGroup className="mb-3 mt-2 password-input-signup">
                                     <FormControl
-                                        // onChange = {this.hadnleChange}
-                                        name = "Password"
-                                        // value = {this.state.data.Password}
-                                        id="password" type="password"
+                                        onChange = {this.handleChange}
+                                        name = "password1"
+                                        value = {this.state.data.password1}
+                                        id="password1" type="password"
                                         className = "placeHolder-input input-style"
-                                        placeholder="رمز عبور خودرا تکرار نمایید"
-                                    />
+                                        placeholder="رمز عبور خودرا وارد نمایید"
+                                        required/>
                                 </InputGroup>
                             </div>
+                        </div>
+                        <div className="col-sm-6">
+                                <div className="row justify-content-start">
+                                    <h2 className="password-input-field mt-4"> تکرار رمزعبور</h2>
+                                </div>
+                                <div className="row justify-content-center">
+                                    <InputGroup className="mb-3 mt-2 password-input-signup">
+                                        <FormControl
+                                            onChange = {this.handleChange}
+                                            name = "password2"
+                                            value = {this.state.data.password2}
+                                            id="password2" type="password"
+                                            className = "placeHolder-input input-style"
+                                            placeholder="رمز عبور خودرا تکرار نمایید"
+                                            required/>
+                                    </InputGroup>
+                                </div>
+                        </div>
                     </div>
-                </div>
-                <div className="row justify-content-center">
-                    <Button id="button" className="mt-4 mb-4 button-submit-signup" type="submit" variant="success" size="md">
-                        ثبت نام
-                    </Button>
-                </div>
+                    <div className="row justify-content-center">
+                        <Button id="button" className="mt-4 mb-4 button-submit-signup" type="submit" variant="success" size="md">
+                            ثبت نام
+                        </Button>
+                    </div>
+                </form>
 
 
 
