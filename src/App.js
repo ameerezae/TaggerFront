bc40729ce416dc2a8f770ecf0d5a1aca7264ad7c
96521227 <amir.reazei1999@yahoo.com>
2019-09-17 17:28:50 +0430

    Change SideNavbar UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LoginContainer from "./container/Login/Login";
 import SignUpContainer from "./container/SignUp/SignUp";
 import ProfileComponent from "./container/Profile/Profile";
+import EditProfileContainer from "./container/EditProfile/EditProfile";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Route path="/login" component={LoginContainer}/>
             <Route path="/signup" component={SignUpContainer}/>
             <Route path="/profile" component={ProfileComponent}/>
+            <Route path ="/edit" component={EditProfileContainer}/>
           </Switch>
         </div>
       </BrowserRouter>
