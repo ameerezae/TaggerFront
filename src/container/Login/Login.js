acb8f57a3f3920d818afb913410960f4aba47861
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-10-09 14:17:53 +0330

    Handle Login error responses

--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -5,17 +5,18 @@ import {InputGroup, FormControl, Button} from 'react-bootstrap';
 
 class LoginContainer extends Component {
 
-    state ={
-        data : {
-            username : "",
+    state = {
+        data: {
+            username: "",
             password: "",
-        }
+        },
+        error: null,
     };
 
-    static async handleSubmit  (event, data) {
+    async handleSubmit(event, data) {
         event.preventDefault();
         try {
-            const url = 'http://localhost:8000/authentication/login/';
+            const url = 'http://127.0.0.1:8000/authentication/login/';
             const response = await fetch(`${url}`, {
                 method: "POST",
                 body: JSON.stringify(data),
@@ -24,15 +25,30 @@ class LoginContainer extends Component {
                     "Content-type": "application/json"
                 }
             });
+            if (response.status === 400) {
+                const data = await response.json();
+                this.showError(data)
+            }
+
             if (!response.ok) {
                 console.log("OHH BadRequest!");
                 throw Error(response.statusText);
             }
-        }catch (e) {
-            console.log(e,"The server is Down!");
+        } catch (e) {
+            console.log(e, "The server is Down!");
         }
     }
-
+    showError = (json) => {
+        if(this.state.data.username.length === 0){
+            this.setState({error : json["non_field_errors"][0]})
+        }
+        else if(this.state.data.password.length === 0){
+            this.setState({error : json["password"][0]})
+        }
+        else {
+            this.setState({error : json["non_field_errors"][0]})
+        }
+    };
     hadnleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
@@ -55,7 +71,9 @@ class LoginContainer extends Component {
                 <hr/>
                 <div className="row direct-to-signup-box">
                     <div className="col-sm-6">
-                        <div className="row justify-content-center guide-to-signup ">اگر حساب کاربری ندارید ثبت نام کنید:</div>
+                        <div className="row justify-content-center guide-to-signup ">اگر حساب کاربری ندارید ثبت نام
+                            کنید:
+                        </div>
                     </div>
                     <div className="col-sm-6">
                         <div className="row justify-content-center link-to-signUp"><a href={"/signup"}>عضویت</a></div>
@@ -64,47 +82,56 @@ class LoginContainer extends Component {
 
                 </div>
                 <hr/>
-                <form onSubmit={event => LoginContainer.handleSubmit(event,this.state.data)}>
-                <div className="row justify-content-end">
-                    <h2 className="email-input-field mt-3">نام کاربری</h2>
-                </div>
-                <div className="row justify-content-center">
-                            <InputGroup  className="mb-3 mt-2 email-input-login">
-                                <FormControl
-                                    onChange = {this.hadnleChange}
-                                    name ="username"
-                                    value = {this.state.data.Email}
-                                    id="email" type="text"
-                                    className = "placeHolder-input input-style"
-                                    placeholder="نام کاربری خودرا وارد نمایید"
-                                />
-                            </InputGroup>
-                </div>
-                <div className="row justify-content-end">
-                    <h2 className="password-input-field mt-4">رمزعبور</h2>
-                </div>
-                <div className="row justify-content-center">
-                    <InputGroup className="mb-3 mt-2 password-input-login">
-                        <FormControl
-                            onChange = {this.hadnleChange}
-                            name = "password"
-                            value = {this.state.data.Password}
-                            id="password" type="password"
-                            className = "placeHolder-input input-style"
-                            placeholder="رمز عبور خودرا وارد نمایید"
-                        />
-                    </InputGroup>
-                </div>
-                <div className="row justify-content-center">
-                    <Button id="button" className="mb-5 mt-4 button-submit-login" type="submit" variant="success" size="md">
-                        ورود
-                    </Button>
-                </div>
+                <form onSubmit={event => this.handleSubmit(event, this.state.data)}>
+                    <div className="row justify-content-end">
+                        <h2 className="email-input-field mt-3">نام کاربری</h2>
+                    </div>
+                    <div className="row justify-content-center">
+                        <InputGroup className="mb-3 mt-2 email-input-login">
+                            <FormControl
+                                onChange={this.hadnleChange}
+                                name="username"
+                                value={this.state.data.Email}
+                                id="email" type="text"
+                                className="placeHolder-input input-style"
+                                placeholder="نام کاربری خودرا وارد نمایید"
+                            />
+                        </InputGroup>
+                    </div>
+                    <div className="row justify-content-end">
+                        <h2 className="password-input-field mt-4">رمزعبور</h2>
+                    </div>
+                    <div className="row justify-content-center">
+                        <InputGroup className="mb-3 mt-2 password-input-login">
+                            <FormControl
+                                onChange={this.hadnleChange}
+                                name="password"
+                                value={this.state.data.Password}
+                                id="password" type="password"
+                                className="placeHolder-input input-style"
+                                placeholder="رمز عبور خودرا وارد نمایید"
+                            />
+                        </InputGroup>
+                    </div>
+                    {this.state.error ?
+                        <div className="row justify-content-center" >
+                            <div className="alert alert-danger" role="alert">
+                                {this.state.error}
+                            </div>
+
+                        </div> :null
+                    }
+                    <div className="row justify-content-center">
+                        <Button id="button" className="mb-5 button-submit-login" type="submit" variant="success"
+                                size="md">
+                            ورود
+                        </Button>
+                    </div>
+
                 </form>
 
             </div>
         );
-
 
 
         return (
@@ -114,6 +141,8 @@ class LoginContainer extends Component {
             </div>
         );
     }
+
+
 }
 
 export default LoginContainer;
