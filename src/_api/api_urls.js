0a78da1c0a20dde9626a5d62226e6913359a6f6d
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-10-29 22:09:59 +0330

    Add edit Profile url

--- src/_api/api_urls.js
+++ src/_api/api_urls.js
@@ -2,4 +2,5 @@ export const MAIN = "http://127.0.0.1:8000";
 export const LOGIN = MAIN + "/authentication/login/";
 export const SIGNUP = MAIN + "/authentication/registration/";
 export const CHECKUSERNAME = MAIN + "/authentication/check_username/";
-export const PROFILE = MAIN + "/profile/";
+export const PROFILE = MAIN + "/authentication/profile/";
+export const EDIT_PROFILE = MAIN + "/authentication/profile/";
