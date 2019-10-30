2d67303acb4101c3b1cce89f57b0577eca60f134
amirRezaeii <amir.rezaei1999@yahoo.com>
2019-10-30 14:42:03 +0330

    Add order fields

--- src/container/CreateOrder/CreateOrder.js
+++ src/container/CreateOrder/CreateOrder.js
@@ -3,12 +3,25 @@ import {FaUser, IoIosList} from "react-icons/all";
 import SideNavbarXs from "../../Components/SideNavbar-xs/SideNavbar-xs";
 import SideNavbarComponent from "../../Components/SideNavbar/SideNavbar";
 import {Select, TextInput} from "react-responsive-ui";
+import NumberFormat from "react-number-format";
 import "./CreateOrder.scss"
 
 class CreateOrderContainer extends Component {
     state = {
         showNav: null,
-    }
+        price: null,
+    };
+
+
+    handleChange = (name, value) => {
+        this.setState(prevState => {
+            const newState = {...prevState};
+            newState[name] = value;
+            return newState;
+        }, () => {
+            console.log(this.state.price)
+        });
+    };
 
     render() {
         return (
@@ -32,7 +45,7 @@ class CreateOrderContainer extends Component {
                                 <h5 className="text-muted m-4">چه نوع سفارشی دارید؟</h5>
                             </div>
                             <div className="row">
-                                <div className="col-sm-8 col-xs-12">
+                                <div className="col-md-8 col-xs-12">
                                     <Select
                                         selectClassName="directionToLeft"
                                         success={"OK"}
@@ -45,13 +58,13 @@ class CreateOrderContainer extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="container">
+                        <div className="container mb-4">
                             <div className="row align-items-center mr-1">
                                 <div className="row align-items-center justify-content-center circle-icon">2</div>
                                 <h5 className="text-muted m-4">عنوان سفارش خودرا بنویسید</h5>
                             </div>
                             <div className="row">
-                                <div className="col-sm-8 col-xs-12">
+                                <div className="col-md-8 col-xs-12">
                                     <TextInput
                                         type="text"
                                         label="عنوان"
@@ -62,6 +75,91 @@ class CreateOrderContainer extends Component {
                                     />
                                 </div>
                             </div>
+                        </div>
+
+                        <div className="container mb-4">
+                            <div className="row align-items-center mr-1">
+                                <div className="row align-items-center justify-content-center circle-icon">3</div>
+                                <h5 className="text-muted m-4">سفارش خود را توضیح دهید.</h5>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-8 col-xs-12">
+                                    <TextInput
+                                        multiline
+                                        type="text"
+                                        label="توضیحات"
+                                        // value={this.state.data.username}
+                                        // onChange={(value) => {
+                                        //     this.hadnleChange("username", value)
+                                        // }}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="container mb-4">
+                            <div className="row align-items-center mr-1">
+                                <div className="row align-items-center justify-content-center circle-icon">4</div>
+                                <h5 className="text-muted m-4">بودجه مورد نظر شما چقدر است؟</h5>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-8 col-xs-12">
+                                    {/*<NumberFormat*/}
+                                    {/*    value={this.state.price}*/}
+                                    {/*    onValueChange={values => {*/}
+                                    {/*        console.log(values);*/}
+                                    {/*    }}*/}
+                                    {/*    customInput = {TextInput}*/}
+                                    {/*    thousandSeparator*/}
+                                    {/*    prefix="تومان"/>*/}
+                                    <TextInput type="number"
+                                               className = "customized-input"
+                                               value={this.state.price}
+                                               onChange={(value) => {
+                                                   this.handleChange("price", value)
+                                               }}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="container mb-4">
+                            <div className="row">
+                                <div className="col-md-4 mb-4">
+                                    <div className="row align-items-center mr-1">
+                                        <div className="row align-items-center justify-content-center circle-icon">5
+                                        </div>
+                                        <h5 className="text-muted m-4"> زمان مورد انتظار شما چقدر است؟</h5>
+                                    </div>
+
+                                    <TextInput type="number"
+                                               className = "customized-input"
+                                               value={this.state.price}
+                                               onChange={(value) => {
+                                                   this.handleChange("price", value)
+                                               }}
+                                    />
+
+                                </div>
+                                <div className="col-md-4 mb-4">
+                                    <div className="row align-items-center mr-1">
+                                        <div className="row align-items-center justify-content-center circle-icon">6
+                                        </div>
+                                        <h5 className="text-muted m-4"> چند نفر روی این پروژه فعالیت کنند؟</h5>
+                                    </div>
+
+                                    <TextInput type="number"
+                                               min="1"
+                                               className = "customized-input"
+                                               value={this.state.price}
+                                               onChange={(value) => {
+                                                   this.handleChange("price", value)
+                                               }}
+                                    />
+
+                                </div>
+                            </div>
+
                         </div>
 
                     </div>
